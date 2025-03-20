4b9eb66dead99b4562764923483060eb77a07f06
PNNhatTram <nhattrampn782004@gmail.com>
2025-03-20 13:30:48 +0700

    find AndroidManifest.xml in folder

--- extract_permission.py
+++ extract_permission.py
@@ -6,11 +6,24 @@ def openFolder():
     folder = input("Enter the folder path for reconnaissance: ").strip()
     # Check if the path is valid
     if os.path.exists(folder):
-        print(folder)
+        searchAndMa(folder)
     else:
         print("The folder does not exist!")
         return openFolder()
-    
+
+""" Find AndroidManifest.xml """
+def searchAndMa(folder):
+    # Loop through all files and directories in the folder
+    for f in os.listdir(folder):
+        if f == "AndroidManifest.xml":
+            try:
+                # Open file (use absolute path) in read mode with UTF-8 encoding
+                with open(os.path.join(folder, f), "r", encoding="utf-8") as file:
+                    print(file.read())
+            except Exception as e:
+                print(f"Error: {e}")
+    print("File does not exist!")
+
 def main():
     openFolder()
 
